09137e5004a39d6d5d86cbb358490335eb549437
Juliana Prieto <jprietoarcila@gmail.com>
2020-12-22 08:37:03 -0500

    Navigation added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,29 @@
 <template>
-  <div>
+  <div class="container">
+    <div class="navigation">
+      <div class="navigation__wrap">
+        <span class="material-icons navigation__menu">
+          menu
+        </span>
+        <div class="navigation__logo">torre</div>
+      </div>
+      <div class="navigation__wrap">
+        <div class="navigation__link">
+          <span class="material-icons navigation__link-icon">
+            psychology
+          </span>
+          <p class="navigation__link-text">Learning</p>
+        </div>
+        <div class="navigation__link">
+          <span class="material-icons navigation__link-icon">
+            stars
+          </span>
+          <p class="navigation__link-text">Showcase</p>
+        </div>
+      </div>
+    </div>
     <Nuxt />
   </div>
 </template>
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+<style></style>
