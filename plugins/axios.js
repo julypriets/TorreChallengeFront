6bba1f2b61b2f52d29f82b73e3d4cc22639da421
Juliana Prieto <jprietoarcila@gmail.com>
2020-12-23 10:08:24 -0500

    Configuration prod

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,6 +1,8 @@
 export default function({ $axios }) {
   // Set baseURL to something different
   const baseURL =
-    process.env.NODE_ENV === "production" ? "" : "http://localhost:8080/api/";
+    process.env.NODE_ENV === "production"
+      ? "'https://torre-challenge.herokuapp.com/api/'"
+      : "http://localhost:8080/api/";
   $axios.setBaseURL(baseURL);
 }
